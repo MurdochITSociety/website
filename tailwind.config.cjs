b18d1e78c6138e62213d46dabe3cf8e52a8a9f42
J319s0 <josh.smith3190@gmail.com>
2022-12-08 18:44:17 +0800

    generic css to tailwind
    changed the generic css used to build website to tailwind css. cleaned up a few areas of program. minor adjustments to page layout

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.astro"],
+  theme: {
+    textColor:
+    {
+      white: "#FFF",
+    },
+    colors:
+    {
+      "grey": "#212121",
+      "black": "#000000",
+      "footer": "#333333"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
