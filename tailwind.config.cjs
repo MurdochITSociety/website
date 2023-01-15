e98b68ba01605b7fa2a4a09d4a6211e6ef8c5f37
J319s0 <josh.smith17090@gmail.com>
2023-01-15 19:39:47 +0800

    header reveal + hide animation and welcome page edit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,27 @@ module.exports = {
       "black": "#000000",
       "footer": "#333333"
     },
-    extend: {},
+    extend: 
+    {
+      keyframes: 
+      {
+        headerHide: 
+        {
+          '0%': { top: '0px' },
+          '100%': { top: '-120px' },
+        },
+        headerReveal:
+        {
+          '0%': {top: '-120px'},
+          '100%': {top: '0px'},
+        }
+      },
+      animation:
+      {
+        'hide': 'headerHide 0.2s linear forwards',
+        'reveal': 'headerReveal 0.2s linear forwards',
+      },
+    },
   },
   plugins: [],
 }
