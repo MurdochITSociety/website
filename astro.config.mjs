1f3f54d615fac432f518a95a4b840bc76de8e063
Wolfleader <25214835+Wolfleader101@users.noreply.github.com>
2023-02-22 10:11:42 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://secondoccurrence.github.io/MITSWebsite/",
-  base: "/MITSWebsite",
+  site: "https://mits.club",
+  base: "/",
   integrations: [tailwind()]
 });
